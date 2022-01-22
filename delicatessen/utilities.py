Fixing partial derivative over-writing error

delicatessen/utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.misc import derivative
+from copy import copy
 
 
 def partial_derivative(func, var, point, output, dx, order):
@@ -25,7 +26,7 @@ def partial_derivative(func, var, point, output, dx, order):
     float
         Partial derivative at the variable at a particular points
     """
-    args = point[:]
+    args = copy(point[:])    # Copy is needed here to prevent over-writing (not sure why SciPy overwrites if not)
 
     def wraps(x):
         args[var] = x