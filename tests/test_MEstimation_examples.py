c42a152fac85765cca1fabdd4ce82c4f4c4512c9
Mark Klose <markklose17@gmail.com>
2022-01-24 23:11:46 -0500

    Wrote tests for Ratio (7.2.3) documentation

--- tests/test_MEstimation_examples.py
+++ tests/test_MEstimation_examples.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-import numpy.testing as npt
+import pandas as pd
 from delicatessen import MEstimator
 
 epsilon = 1.0E-6
@@ -25,4 +25,45 @@ class TestMEstimationExamples:
     # Ratio 7.2.3 --> typo in equation?
     # Y_i - \theta_1 X_i
     def test_ratio(self):
+        n = 10000
+        y_loc = 10
+        x_loc = 5
+        data = pd.DataFrame()
+        data['Y'] = np.random.normal(loc=y_loc, scale=2, size=n)
+        data['X'] = np.random.normal(loc=x_loc, scale=1, size=n)
+
+        def psi(theta):
+            return data['Y'] - data['X'] * theta
+
+        mestimate = MEstimator(psi, init=[1, ])
+        mestimate.estimate()
+
+        assert mestimate.theta[0] - data['Y'].mean() / data['X'].mean() < epsilon
+
+        def psi(theta):
+            mean_y = data['Y'] - theta[0]
+            mean_x = data['X'] - theta[1]
+            ratio = np.ones(data.shape[0]) * (theta[0] - theta[1] * theta[2])
+            return mean_y, mean_x, ratio
+
+        mestimate = MEstimator(psi, init=[0, 0, 1])
+        mestimate.estimate()
+
+        assert mestimate.theta[0] - data['Y'].mean() < epsilon
+        assert mestimate.theta[1] - data['X'].mean() < epsilon
+        assert mestimate.theta[2] - data['Y'].mean() / data['X'].mean() < epsilon
+
+    def test_delta_method(self):
+        pass
+
+    def test_instrumental_variable(self):
+        pass
+
+    def test_robust_location(self):
+        pass
+
+    def test_linear_regression(self):
+        pass
+
+    def test_gee(self):
         pass
