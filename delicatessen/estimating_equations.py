58e9d3c909845c648e700b0b4387511baec6f6a6
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2022-01-27 12:46:11 -0500

    add examples to PL models

--- delicatessen/estimating_equations.py
+++ delicatessen/estimating_equations.py
@@ -757,6 +757,70 @@ def ee_4p_logistic(theta, X, y):
     --------
     Construction of a estimating equation(s) with `ee_4p_logistic` should be done similar to the following
 
+    >>> from delicatessen import MEstimator
+    >>> from delicatessen.data import load_inderjit
+    >>> from delicatessen.estimating_equations import ee_4p_logistic
+
+    For demonstration, we use dose-response data from Inderjit et al. (2002), which can be loaded from ``delicatessen``
+    directly.
+
+    >>> d = load_inderjit()   # Loading array of data
+    >>> dose_data = d[:, 1]   # Dose data
+    >>> resp_data = d[:, 0]   # Response data
+
+    Defining psi, or the stacked estimating equations
+
+    >>> def psi(theta):
+    >>>     return ee_4p_logistic(theta=theta, X=dose_data, y=resp_data)
+
+    The 4PL model and others are harder to optimize compared to other estimating equations. Namely, the optimizer is
+    not aware of implicit bounds on the parameters. To reduce non-convergence issues, we can give the root-finder good
+    starting values.
+
+    For the 4PL, the upper limit should *always* be greater than the lower limit. Second, the ED50 should be between
+    the lower and upper limits. Third, the sign for the steepness depends on whether the response declines (positive)
+    or the response increases (negative). Finally, some solvers may be better suited to the problem, so try a few
+    different options.
+
+    Here, we use some general starting values that should perform well in many cases. For the lower-bound, give the
+    minimum response value as the initial. For ED50, give the mid-point between the maximum response and the minimum
+    response. The initial value for steepness is more difficult. Ideally, we would give a starting value of zero, but
+    that will fail in this example. Giving a small positive starting value works in this example. For the upper-bound,
+    give the maximum response value as the initial. Finally, we use the ``lm`` solver.
+
+    Note
+    ----
+    To summarize the recommendations, be sure to examine your data (e.g., scatterplot). This will help to determine the
+    initial starting values for the root-finding procedure. Otherwise, you may come across a convergence error.
+
+    >>> estr = MEstimator(psi, init=[np.min(resp_data),
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              np.max(resp_data)])
+    >>> estr.estimate(solver='lm')
+
+    Inspecting the parameter estimates, variance, and confidence intervals
+
+    >>> estr.theta
+    >>> estr.variance
+    >>> estr.confidence_intervals()
+
+    Inspecting the parameter estimates
+
+    >>> estr.theta[0]    # lower limit
+    >>> estr.theta[1]    # ED(50)
+    >>> estr.theta[2]    # steepness
+    >>> estr.theta[3]    # upper limit
+
+    References
+    ----------
+    Ritz C, Baty F, Streibig JC, & Gerhard D. (2015). Dose-response analysis using R. *PloS One*, 10(12), e0146021.
+
+    An H, Justin TL, Aubrey GB, Marron JS, & Dittmer DP. (2019). dr4pl: A Stable Convergence Algorithm for the 4
+    Parameter Logistic Model. *R J.*, 11(2), 171.
+
+    Inderjit, Streibig JC, & Olofsdotter M. (2002). Joint action of phenolic acid mixtures and its significance in
+    allelopathy research. *Physiologia Plantarum*, 114(3), 422-428.
     """
     # Creating rho to cut down on typing
     rho = (X / theta[1]) ** theta[2]
@@ -821,6 +885,70 @@ def ee_3p_logistic(theta, X, y, lower):
     --------
     Construction of a estimating equation(s) with `ee_3p_logistic` should be done similar to the following
 
+    >>> from delicatessen import MEstimator
+    >>> from delicatessen.data import load_inderjit
+    >>> from delicatessen.estimating_equations import ee_3p_logistic
+
+    For demonstration, we use dose-response data from Inderjit et al. (2002), which can be loaded from ``delicatessen``
+    directly.
+
+    >>> d = load_inderjit()   # Loading array of data
+    >>> dose_data = d[:, 1]   # Dose data
+    >>> resp_data = d[:, 0]   # Response data
+
+    Since there is a natural lower-bound of 0 for root growth, we set ``lower=0``. Defining psi, or the stacked
+    estimating equations
+
+    >>> def psi(theta):
+    >>>     return ee_3p_logistic(theta=theta, X=dose_data, y=resp_data,
+    >>>                           lower=0)
+
+    The 3PL model and others are harder to optimize compared to other estimating equations. Namely, the optimizer is
+    not aware of implicit bounds on the parameters. To reduce non-convergence issues, we can give the root-finder good
+    starting values.
+
+    For the 3PL, the upper limit should *always* be greater than the set lower limit. Second, the ED50 should be between
+    the lower and upper limits. Third, the sign for the steepness depends on whether the response declines (positive)
+    or the response increases (negative). Finally, some solvers may be better suited to the problem, so try a few
+    different options.
+
+    Here, we use some general starting values that should perform well in many cases. For ED50, give the mid-point
+    between the maximum response and the minimum response. The initial value for steepness is more difficult. Ideally,
+    we would give a starting value of zero, but that will fail in this example. Giving a small positive starting value
+    works in this example. For the upper-bound, give the maximum response value as the initial. Finally, we use the
+    ``lm`` solver.
+
+    Note
+    ----
+    To summarize the recommendations, be sure to examine your data (e.g., scatterplot). This will help to determine the
+    initial starting values for the root-finding procedure. Otherwise, you may come across a convergence error.
+
+    >>> estr = MEstimator(psi, init=[(np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              np.max(resp_data)])
+    >>> estr.estimate(solver='lm')
+
+    Inspecting the parameter estimates, variance, and confidence intervals
+
+    >>> estr.theta
+    >>> estr.variance
+    >>> estr.confidence_intervals()
+
+    Inspecting the parameter estimates
+
+    >>> estr.theta[0]    # ED(50)
+    >>> estr.theta[1]    # steepness
+    >>> estr.theta[2]    # upper limit
+
+    References
+    ----------
+    Ritz C, Baty F, Streibig JC, & Gerhard D. (2015). Dose-response analysis using R. *PloS One*, 10(12), e0146021.
+
+    An H, Justin TL, Aubrey GB, Marron JS, & Dittmer DP. (2019). dr4pl: A Stable Convergence Algorithm for the 4
+    Parameter Logistic Model. *R J.*, 11(2), 171.
+
+    Inderjit, Streibig JC, & Olofsdotter M. (2002). Joint action of phenolic acid mixtures and its significance in
+    allelopathy research. *Physiologia Plantarum*, 114(3), 422-428.
     """
     # Creating rho to cut down on typing
     rho = (X / theta[0])**theta[1]
@@ -886,6 +1014,67 @@ def ee_2p_logistic(theta, X, y, lower, upper):
     --------
     Construction of a estimating equation(s) with `ee_2p_logistic` should be done similar to the following
 
+    >>> from delicatessen import MEstimator
+    >>> from delicatessen.data import load_inderjit
+    >>> from delicatessen.estimating_equations import ee_2p_logistic
+
+    For demonstration, we use dose-response data from Inderjit et al. (2002), which can be loaded from ``delicatessen``
+    directly.
+
+    >>> d = load_inderjit()   # Loading array of data
+    >>> dose_data = d[:, 1]   # Dose data
+    >>> resp_data = d[:, 0]   # Response data
+
+    Since there is a natural lower-bound of 0 for root growth, we set ``lower=0``. While a natural upper bound does not
+    exist for this example, we set ``upper=8`` for illustrative purposes. Defining psi, or the stacked estimating
+    equations
+
+    >>> def psi(theta):
+    >>>     return ee_2p_logistic(theta=theta, X=dose_data, y=resp_data,
+    >>>                           lower=0, upper=8)
+
+    The 2PL model and others are harder to optimize compared to other estimating equations. Namely, the optimizer is
+    not aware of implicit bounds on the parameters. To reduce non-convergence issues, we can give the root-finder good
+    starting values.
+
+    First, the ED50 should be between the lower and upper limits. Second, the sign for the steepness depends on whether
+    the response declines (positive) or the response increases (negative). Finally, some solvers may be better suited
+    to the problem, so try a few different options.
+
+    Here, we use some general starting values that should perform well in many cases. For ED50, give the mid-point
+    between the maximum response and the minimum response. The initial value for steepness is more difficult. Ideally,
+    we would give a starting value of zero, but that will fail in this example. Giving a small positive starting value
+    works in this example. Finally, we use the ``lm`` solver.
+
+    Note
+    ----
+    To summarize the recommendations, be sure to examine your data (e.g., scatterplot). This will help to determine the
+    initial starting values for the root-finding procedure. Otherwise, you may come across a convergence error.
+
+    >>> estr = MEstimator(psi, init=[(np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2])
+    >>> estr.estimate(solver='lm')
+
+    Inspecting the parameter estimates, variance, and confidence intervals
+
+    >>> estr.theta
+    >>> estr.variance
+    >>> estr.confidence_intervals()
+
+    Inspecting the parameter estimates
+
+    >>> estr.theta[0]    # ED(50)
+    >>> estr.theta[1]    # steepness
+
+    References
+    ----------
+    Ritz C, Baty F, Streibig JC, & Gerhard D. (2015). Dose-response analysis using R. *PloS One*, 10(12), e0146021.
+
+    An H, Justin TL, Aubrey GB, Marron JS, & Dittmer DP. (2019). dr4pl: A Stable Convergence Algorithm for the 4
+    Parameter Logistic Model. *R J.*, 11(2), 171.
+
+    Inderjit, Streibig JC, & Olofsdotter M. (2002). Joint action of phenolic acid mixtures and its significance in
+    allelopathy research. *Physiologia Plantarum*, 114(3), 422-428.
     """
     # Creating rho to cut down on typing
     rho = (X / theta[0])**theta[1]
@@ -949,8 +1138,64 @@ def ee_effective_dose_delta(theta, y, delta, steepness, ed50, lower, upper):
 
     Examples
     --------
-    Construction of a estimating equations for ED25 with `ee_4p_logistic` should be done similar to the following
+    Construction of a estimating equations for ED25 with `ee_3p_logistic` should be done similar to the following
 
+    >>> from delicatessen import MEstimator
+    >>> from delicatessen.data import load_inderjit
+    >>> from delicatessen.estimating_equations import ee_2p_logistic, ee_effective_dose_delta
+
+    For demonstration, we use dose-response data from Inderjit et al. (2002), which can be loaded from ``delicatessen``
+    directly.
+
+    >>> d = load_inderjit()   # Loading array of data
+    >>> dose_data = d[:, 1]   # Dose data
+    >>> resp_data = d[:, 0]   # Response data
+
+    Since there is a natural lower-bound of 0 for root growth, we set ``lower=0``. While a natural upper bound does not
+    exist for this example, we set ``upper=8`` for illustrative purposes. Defining psi, or the stacked estimating
+    equations
+
+    >>> def psi(theta):
+    >>>     pl_model = ee_3p_logistic(theta=theta, X=dose_data, y=resp_data,
+    >>>                               lower=0)
+    >>>     ed_25 = ee_effective_dose_delta(theta[3], y=resp_data, delta=0.20,
+    >>>                                     steepness=theta[0], ed50=theta[1],
+    >>>                                     lower=0, upper=theta[2])
+    >>>     # Returning stacked estimating equations
+    >>>     return np.vstack((pl_model,
+    >>>                       ed_25,))
+
+    Notice that the estimating equations are stacked in the order of the parameters in ``theta`` (the first 3 belong to
+    3PL and the last belong to ED(25)).
+
+    >>> estr = MEstimator(psi, init=[(np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2,
+    >>>                              np.max(resp_data),
+    >>>                              (np.max(resp_data)+np.min(resp_data)) / 2])
+    >>> estr.estimate(solver='lm')
+
+    Inspecting the parameter estimates, variance, and confidence intervals
+
+    >>> estr.theta
+    >>> estr.variance
+    >>> estr.confidence_intervals()
+
+    Inspecting the parameter estimates
+
+    >>> estr.theta[0]    # ED(50)
+    >>> estr.theta[1]    # steepness
+    >>> estr.theta[2]    # upper limit
+    >>> estr.theta[3]    # ED(25)
+
+    References
+    ----------
+    Ritz C, Baty F, Streibig JC, & Gerhard D. (2015). Dose-response analysis using R. *PloS One*, 10(12), e0146021.
+
+    An H, Justin TL, Aubrey GB, Marron JS, & Dittmer DP. (2019). dr4pl: A Stable Convergence Algorithm for the 4
+    Parameter Logistic Model. *R J.*, 11(2), 171.
+
+    Inderjit, Streibig JC, & Olofsdotter M. (2002). Joint action of phenolic acid mixtures and its significance in
+    allelopathy research. *Physiologia Plantarum*, 114(3), 422-428.
     """
     # Creating rho to cut down on typing
     rho = (theta / steepness)**ed50            # Theta is the corresponds ED(alpha) value
