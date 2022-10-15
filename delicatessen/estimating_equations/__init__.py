186280d8fb6dcc0fd3f127ff28c66fa54f0cd9db
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2022-10-15 13:18:33 -0400

    removing old regressions from import

--- delicatessen/estimating_equations/__init__.py
+++ delicatessen/estimating_equations/__init__.py
@@ -6,9 +6,9 @@ from .causal import (ee_ipw, ee_gformula, ee_aipw)
 from .dose_response import (ee_4p_logistic, ee_3p_logistic, ee_2p_logistic,
                             ee_effective_dose_delta)
 
-from .regression import (ee_regression, ee_linear_regression, ee_logistic_regression, ee_poisson_regression,
-                         ee_ridge_regression, ee_lasso_regression, ee_elasticnet_regression, ee_bridge_regression,
-                         ee_robust_regression, ee_robust_linear_regression)
+from .regression import (ee_regression,
+                         ee_robust_regression,
+                         ee_ridge_regression, ee_lasso_regression, ee_elasticnet_regression, ee_bridge_regression)
 
 from .survival import (ee_exponential_model, ee_weibull_model,
                        ee_exponential_measure, ee_weibull_measure,
