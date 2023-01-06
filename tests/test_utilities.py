61744dba05851f8a7c4051da51bdb00709a5dbb3
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2023-01-06 15:15:22 -0500

    tests for identity, splines, additive design matrix

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,10 +2,28 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 
-from delicatessen.utilities import inverse_logit, logit, robust_loss_functions
+from delicatessen.utilities import (identity, inverse_logit, logit,
+                                    robust_loss_functions,
+                                    spline,
+                                    additive_design_matrix)
+
+
+@pytest.fixture
+def design_matrix():
+    array = np.array([[1, 1, 1, 1, 1],
+                      [1, 5, 10, 15, 20],
+                      [1, 2, 3, 4, 5], ])
+    return array.T
 
 
 class TestFunctions:
+
+    def test_identity_var(self):
+        """Checks the logit transformation of a single observation
+        """
+        npt.assert_allclose(identity(0.5), 0.5)
+        npt.assert_allclose(identity(0.25), 0.25)
+        npt.assert_allclose(identity(0.75), 0.75)
 
     def test_logit_var(self):
         """Checks the logit transformation of a single observation
@@ -40,6 +58,12 @@ class TestFunctions:
                             original)
         npt.assert_allclose(logit(inverse_logit(logit(inverse_logit(logit(inverse_logit(original_logit)))))),
                             original_logit)
+
+    def test_identity_array(self):
+        """Checks the inverse logit transformation of an array
+        """
+        vals = np.array([0.5, 0.25, 0.75, 0.5])
+        npt.assert_allclose(identity(vals), vals)
 
     def test_logit_array(self):
         """Checks the inverse logit transformation of an array
@@ -136,3 +160,158 @@ class TestFunctions:
         with pytest.raises(ValueError, match="requires that a < b < k"):
             robust_loss_functions(residual=residuals, loss='hampel',
                                   k=1.5, a=1, b=2)
+
+    def test_spline1(self):
+        vars = [1, 5, 10, 15, 20]
+
+        # Spline setup 1
+        expected = np.array([[0.0, 0.0, 0.0, 0.0, 4.0], ]).T
+        returned = spline(variable=vars, knots=[16, ], power=1, restricted=False)
+        npt.assert_allclose(returned, expected)
+
+        # Spline setup 2
+        expected = np.array([[0.0, 0.0, 0.0, 0.0, 16.0], ]).T
+        returned = spline(variable=vars, knots=[16, ], power=2, restricted=False)
+        npt.assert_allclose(returned, expected)
+
+        # Spline setup 3
+        expected = np.array([[0.0, 0.0, 0.0, 5.0**1.5, 10.0**1.5],
+                             [0.0, 0.0, 0.0, 0.0, 4.0**1.5]]).T
+        returned = spline(variable=vars, knots=[10, 16], power=1.5, restricted=False)
+        npt.assert_allclose(returned, expected)
+
+        # Spline setup 4
+        expected = np.array([[0.0, 0.0, 0.0, 5.0, 6.0], ]).T
+        returned = spline(variable=vars, knots=[10, 16], power=1, restricted=True)
+        npt.assert_allclose(returned, expected)
+
+        # Spline setup 5
+        expected = np.array([[0.0, 0.0, 5.0**2, 10.0**2, 15.0**2 - 4.0**2], ]).T
+        returned = spline(variable=vars, knots=[5, 16], power=2, restricted=True)
+        npt.assert_allclose(returned, expected)
+
+    def test_adm_error_noknots(self, design_matrix):
+        # Testing error when dictionary with no knots given
+        specs = [None,
+                 {"power": 2},
+                 None]
+        with pytest.raises(ValueError, match="`knots` must be"):
+            additive_design_matrix(X=design_matrix,
+                                   specifications=specs,
+                                   return_penalty=False)
+
+    def test_adm_error_negknots(self, design_matrix):
+        # Testing error when dictionary with negative number of knots given
+        specs = [None,
+                 {"knots": -2},
+                 None]
+        with pytest.raises(ValueError, match="`knots` must be a pos"):
+            additive_design_matrix(X=design_matrix,
+                                   specifications=specs,
+                                   return_penalty=False)
+
+    def test_adm_error_misalign(self, design_matrix):
+        # Testing error matrix is misaligned
+        # Testing warning for extra spline arguments
+        specs = [None,
+                 {"knots": 2},
+                 None]
+        with pytest.raises(ValueError, match="number of input"):
+            additive_design_matrix(X=design_matrix.T,
+                                   specifications=specs,
+                                   return_penalty=False)
+
+    def test_adm_warn_extras(self, design_matrix):
+        # Testing warning for extra spline arguments
+        specs = [None,
+                 {"knots": 2, "extra": 4},
+                 None]
+        with pytest.warns(UserWarning, match="following keys"):
+            additive_design_matrix(X=design_matrix,
+                                   specifications=specs,
+                                   return_penalty=False)
+
+    def test_adm_defaults(self, design_matrix):
+        # Testing default additive design matrix specifications
+        specs = [None,
+                 {"knots": [5, 16]},
+                 {"knots": [2, 4]}]
+        expected_matrix = np.array([[1, 1, 1, 1, 1],
+                                    [1, 5, 10, 15, 20],
+                                    [0.0, 0.0, 5.0**3, 10.0**3, 15.0**3 - 4.0**3],
+                                    [1, 2, 3, 4, 5],
+                                    [0, 0, 1, 2**3, 3**3 - 1]]).T
+        expected_penalty = np.array([0, 0, 0, 0, 0])
+        returned_matrix, returned_penalty = additive_design_matrix(X=design_matrix,
+                                                                   specifications=specs,
+                                                                   return_penalty=True)
+        npt.assert_allclose(returned_matrix, expected_matrix)
+        npt.assert_allclose(returned_penalty, expected_penalty)
+
+    def test_adm_v1(self, design_matrix):
+        specs = [None,
+                 {"knots": [16, ], "power": 1, "natural": False},
+                 None]
+        expected = np.array([[1, 1, 1, 1, 1],
+                             [1, 5, 10, 15, 20],
+                             [0, 0, 0, 0, 4],
+                             [1, 2, 3, 4, 5]]).T
+        returned = additive_design_matrix(X=design_matrix,
+                                          specifications=specs,
+                                          return_penalty=False)
+        npt.assert_allclose(returned, expected)
+
+    def test_adm_v2(self, design_matrix):
+        specs = [None,
+                 {"knots": [16, ], "power": 1, "natural": False},
+                 {"knots": [2, 4], "power": 2, "natural": True}]
+        expected = np.array([[1, 1, 1, 1, 1],
+                             [1, 5, 10, 15, 20],
+                             [0, 0, 0, 0, 4],
+                             [1, 2, 3, 4, 5],
+                             [0, 0, 1, 4, 3**2 - 1]]).T
+        returned = additive_design_matrix(X=design_matrix,
+                                          specifications=specs,
+                                          return_penalty=False)
+        npt.assert_allclose(returned, expected)
+
+    def test_adm_v3(self, design_matrix):
+        # Testing non-ascending order correction for knots
+        specs = [None,
+                 {"knots": [16, ], "power": 1, "natural": False},
+                 {"knots": [4, 2], "power": 2, "natural": True}]
+        expected = np.array([[1, 1, 1, 1, 1],
+                             [1, 5, 10, 15, 20],
+                             [0, 0, 0, 0, 4],
+                             [1, 2, 3, 4, 5],
+                             [0, 0, 1, 4, 3**2 - 1]]).T
+        returned = additive_design_matrix(X=design_matrix,
+                                          specifications=specs,
+                                          return_penalty=False)
+        npt.assert_allclose(returned, expected)
+
+    def test_adm_v4(self, design_matrix):
+        # Testing number of generated rows
+        specs = [None,
+                 {"knots": [16, ], "power": 2, "natural": False},
+                 {"knots": [2, 4], "power": 1, "natural": False}]
+        expected = np.array([[1, 1, 1, 1, 1],
+                             [1, 5, 10, 15, 20],
+                             [0, 0, 0, 0, 4**2],
+                             [1, 2, 3, 4, 5],
+                             [0, 0, 1, 2, 3],
+                             [0, 0, 0, 0, 1]]).T
+        returned = additive_design_matrix(X=design_matrix,
+                                          specifications=specs,
+                                          return_penalty=False)
+        npt.assert_allclose(returned, expected)
+
+    def test_adm_penalty(self, design_matrix):
+        specs = [None,
+                 {"knots": [16, ], "power": 1, "natural": False, "penalty": 3},
+                 {"knots": [2, 3, 4], "power": 2, "natural": True, "penalty": 5}]
+        expected = np.array([0, 0, 3, 0, 5, 5])
+        design, returned = additive_design_matrix(X=design_matrix,
+                                                  specifications=specs,
+                                                  return_penalty=True)
+        npt.assert_allclose(returned, expected)
